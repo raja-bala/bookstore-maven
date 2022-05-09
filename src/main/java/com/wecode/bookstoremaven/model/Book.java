9b6fea895a0ed0bef1417e62952319ab907de90a
raja-bala <bharathiraja.techie@gmail.com>
2022-05-10 00:03:59 +0800

    environment specific config added

--- src/main/java/com/wecode/bookstoremaven/model/Book.java
+++ src/main/java/com/wecode/bookstoremaven/model/Book.java
@@ -2,6 +2,7 @@ package com.wecode.bookstoremaven.model;
 
 import com.sun.istack.NotNull;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 
 @Entity
 @Builder
+@Data
 public class Book {
     public Book() {
     }
--- src/main/resources/db/migration/V1__Book_Schema.sql
+++ src/main/resources/db/migration/V1__Book_Schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE  book(
-    id IDENTITY NOT NULL PRIMARY KEY,
+    id serial PRIMARY KEY,
     title varchar NOT NULL,
     description varchar NOT NULL,
     release_year INT NOT NULL
